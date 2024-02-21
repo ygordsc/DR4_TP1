0ede97ca564d11df90a51a327967e201e7f06c88
Ygor Cardoso <ygor.cardoso@live.com>
2024-02-20 22:38:21 -0400

    Adicionar arquivos de projeto.

---
+++ DR4_TP1/Pessoa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DR4_TP1
+{
+    internal class Pessoa
+    {
+        private string nome;
+        private int idade;
+        private int CPF;
+
+        public string Nome { get => nome; set => nome = value; }
+        public int Idade { get => idade; set => idade = value; }
+        public int CPF1 { get => CPF; set => CPF = value; }
+        
+        public void exibir()
+        {
+            Console.WriteLine(this.Nome);
+            Console.WriteLine(this.Idade);
+            Console.WriteLine(this.CPF1);
+        }
+        
+    }
+}
---
+++ DR4_TP1/Program.cs
@@ -0,0 +1,45 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace DR4_TP1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Console.WriteLine("Olá, mundo!");
+
+            /* Pessoa p1 = new Pessoa();
+
+            p1.Nome = "Ygor";
+            p1.Idade = 18;
+            p1.CPF1 = 123456789;
+
+            p1.exibir(); */
+
+            Console.WriteLine("Adicionar nova pessoa - 1");
+            Console.WriteLine("Sair do programa - 0");
+            int test = Int32.Parse(Console.ReadLine());
+
+            while (test == 1) {
+                Pessoa pessoa = new Pessoa();
+                Console.WriteLine("Digite o nome:");
+                pessoa.Nome = Console.ReadLine();
+
+                Console.WriteLine("Digite a idade:");
+                pessoa.Idade = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite o CPF:");
+                pessoa.CPF1 = Int32.Parse(Console.ReadLine());
+
+                pessoa.exibir();
+
+                Console.WriteLine("Adicionar nova pessoa - 1");
+                Console.WriteLine("Sair do programa - 0");
+                test = Int32.Parse(Console.ReadLine());
+
+            }
+
+        }
+    }
+}
